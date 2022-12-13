af2c5fcf2c060d3ec113842819639d95187bb57f
byval <byvaltsev1978@gmail.com>
2022-12-13 22:44:44 +0300

    Initial commit

---
+++ src/test/java/com/byval/labs/railwaytransport/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byval.labs.railwaytransport.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
